285b9a288336daa914d9578dd48ddb38e89b3a5b
David López <david.lopez@metadrop.net>
2018-07-30 16:01:27 +0200

    Do not create the database on the image

--- ci_prepare_container.sh
+++ ci_prepare_container.sh
@@ -9,9 +9,6 @@ echo -e "memory_limit = 512M\n" > /usr/local/etc/php/conf.d/90-md.ini
 echo -e "\n\nStarting Database"
 service mysql start
 
-echo -e "\n\nCreate an empty database"
-echo "CREATE DATABASE drupal; CREATE USER 'drupal'@'localhost' IDENTIFIED BY 'drupal'; GRANT ALL ON drupal.* TO 'drupal'@'localhost';" | mysql
-
 echo "Install chrome"
 apt-get update
 apt-get install -y wget
